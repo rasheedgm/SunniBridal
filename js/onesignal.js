621cdcc6e16b9b5d72272d772581060e0289d21b
rasheedgm <gm.abdul@gmail.com>
2017-10-14 11:43:14 +0530

    onesignal
    test

---
+++ js/onesignal.js
@@ -0,0 +1,17 @@
+document.addEventListener('deviceready', function () {
+  // Enable to debug issues.
+  // window.plugins.OneSignal.setLogLevel({logLevel: 4, visualLevel: 4});
+  
+  var notificationOpenedCallback = function(jsonData) {
+    console.log('notificationOpenedCallback: ' + JSON.stringify(jsonData));
+  };
+
+  window.plugins.OneSignal
+    .startInit("6a2d9489-1305-435c-9777-e6ff4dc31f74")
+    .handleNotificationOpened(notificationOpenedCallback)
+    .endInit();
+  
+  // Call syncHashedEmail anywhere in your app if you have the user's email.
+  // This improves the effectiveness of OneSignal's "best-time" notification scheduling feature.
+  // window.plugins.OneSignal.syncHashedEmail(userEmail);
+}, false);
