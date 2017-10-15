f1d3affd3466ac811fccd072d6d393a16d1a1d60
rasheedgm <gm.abdul@gmail.com>
2017-10-15 21:17:10 +0530

    Revert "one signal"
    This reverts commit b7791bf2b2fec32372d7eaea8777a903dcb9ab9f.

--- www/js/index.js
+++ www/js/index.js
@@ -27,23 +27,6 @@ var app = {
     // 'load', 'deviceready', 'offline', and 'online'.
     bindEvents: function() {
         document.addEventListener('deviceready', this.onDeviceReady, false);
-        document.addEventListener('deviceready', function () {
-  // Enable to debug issues.
-  // window.plugins.OneSignal.setLogLevel({logLevel: 4, visualLevel: 4});
-  
-  var notificationOpenedCallback = function(jsonData) {
-    console.log('notificationOpenedCallback: ' + JSON.stringify(jsonData));
-  };
-
-  window.plugins.OneSignal
-    .startInit("6a2d9489-1305-435c-9777-e6ff4dc31f74")
-    .handleNotificationOpened(notificationOpenedCallback)
-    .endInit();
-  
-  // Call syncHashedEmail anywhere in your app if you have the user's email.
-  // This improves the effectiveness of OneSignal's "best-time" notification scheduling feature.
-  // window.plugins.OneSignal.syncHashedEmail(userEmail);
-}, false);
     },
     // deviceready Event Handler
     //
@@ -52,7 +35,6 @@ var app = {
     onDeviceReady: function() {
         app.receivedEvent('deviceready');
         window.plugins.PushbotsPlugin.initialize("59e31fe09b823a29548b4594", {"android":{"sender_id":"17733426527"}});
-        
     },
     // Update DOM on a Received Event
     receivedEvent: function(id) {
